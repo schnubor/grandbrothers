666307a75df6b43cf667fcdd7a6f08caffb45ef7
Christian Korndörfer <info@chko.org>
2014-11-23 03:07:57 +0100

    added migrations for users table

---
+++ app/database/migrations/2014_11_23_015529_add_email_to_users_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class AddEmailToUsersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('users', function(Blueprint $table)
+		{
+			$table->string('email');
+			$table->string('remember_token');
+		});
+
+		User::firstOrCreate([
+			'username' => 'admin',
+			'password' => Hash::make('admin'),
+			'email' => 'admin@example.com'
+		]);
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('users', function(Blueprint $table)
+		{
+			
+		});
+	}
+
+}
--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('homestead','Christians-MacBook-Pro-2.local'),
+	'local' => array('homestead','Christians-MacBook-Pro-2.local','christiansmbp2'),
 
 ));
 
