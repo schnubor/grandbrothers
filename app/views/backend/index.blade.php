9273cf676b49faf3aa613e56464b47149752f36a
Christian Korndörfer <info@chko.org>
2014-11-28 20:46:46 +0100

    reinitial commit

--- app/views/backend/index.blade.php
+++ app/views/backend/index.blade.php
@@ -1,10 +1,10 @@
 @extends('layout.default')
 
 @section('content')
+  <div class="jumbotron">
     <div class="container">
-        <div class="jumbotron">
-            <h1>Backend</h1>
-            <p>Manage texts, tourdates and pictures all by yourself.</p>
-        </div>
+      <h1>Backend</h1>
+      <p>Manage texts, tourdates and pictures all by yourself.</p>
     </div>
+  </div>
 @stop
