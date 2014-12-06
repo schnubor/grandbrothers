ddb22cbe6aec561ffdb8231f2b169fa174e493a4
Christian Korndörfer <info@chko.org>
2014-12-06 15:53:27 +0100

    gogo frontend

--- app/views/layout/default.blade.php
+++ app/views/layout/default.blade.php
@@ -1,12 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <title>Grandbrothers</title>
-    <?= stylesheet_link_tag() ?>
-    <?= javascript_include_tag() ?>
+  <meta charset="UTF-8">
+  <title>Grandbrothers</title>
+  <?= stylesheet_link_tag() ?>
+  <?= javascript_include_tag() ?>
 </head>
 <body>
-    @yield('content')
+  <!-- Navigation -->
+  <nav>
+    <ul>
+      <li>News</li>
+      <li>Info</li>
+      <li>Music</li>
+      <li>Live</li>
+      <li>Contact</li>
+    </ul>
+  </nav>
+  @yield('content')
 </body>
 </html>
--- app/views/pages/home.blade.php
+++ app/views/pages/home.blade.php
@@ -1,28 +1,28 @@
 @extends('layout.default')
 
 @section('content')
-    <div class="container">
-        <h1>Home</h1>
-        <h2>News</h2>
-          <ul>
-            @foreach($posts as $post)
-              <li>
-                <h3>{{ $post->title }}</h3>
-                <small>by {{ $post->author }}</small>
-                <p>{{ $post->body }}</p>
-              </li>
-            @endforeach
-          </ul>
-        <h2>Dates</h2>
-          <ul>
-            @foreach($dates as $date)
-              <li>
-                <h3>{{ $date->title }}</h3>
-                <h4>{{ $date->date }}, {{ $date->location }}, {{ $date->price }}€</h4>
-                <p>{{ $date->body }}</p>
+  <div class="container">
+    <h1>Home</h1>
+    <h2>News</h2>
+      <ul>
+        @foreach($posts as $post)
+          <li>
+            <h3>{{ $post->title }}</h3>
+            <small>by {{ $post->author }}</small>
+            <p>{{ $post->body }}</p>
+          </li>
+        @endforeach
+      </ul>
+    <h2>Dates</h2>
+    <ul>
+      @foreach($dates as $date)
+        <li>
+          <h3>{{ $date->title }}</h3>
+          <h4>{{ $date->date }}, {{ $date->location }}, {{ $date->price }}€</h4>
+          <p>{{ $date->body }}</p>
 
-              </li>
-            @endforeach
-          </ul>
-    </div>
+        </li>
+      @endforeach
+    </ul>
+  </div>
 @stop
