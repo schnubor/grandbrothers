729b2645b193e4e2fb58a28a51aaba3a7fda1166
schnubor <info@chko.org>
2015-02-25 00:33:10 +0100

    huge refractor

--- app/views/pages/home.blade.php
+++ app/views/pages/home.blade.php
@@ -4,12 +4,8 @@
 <div id="wrapper">
   <!-- Left Side -->
   <div id="left">
-    <section class="welcome left">
-      <?= image_tag('home1.jpg', ['alt' => 'portrait', 'class' => 'contentImg']) ?>
-    </section>
-    <section class="listen left">
-      <?= image_tag('listen.jpg', ['alt' => 'portrait', 'class' => 'contentImg']) ?>
-    </section>
+    <section class="welcome left"></section>
+    <section class="listen left"></section>
     <section class="news left">
       <div class="content">
         @if(isset($post))
@@ -19,17 +15,13 @@
         @endif
       </div>
     </section>
-    <section class="gigs left">
-      <?= image_tag('live.jpg', ['alt' => 'portrait', 'class' => 'contentImg']) ?>
-    </section>
+    <section class="gigs left"></section>
     <section class="info left">
       <div class="content">
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae dolorem modi nam veniam rerum doloremque, illum harum natus incidunt numquam necessitatibus animi, autem quidem accusantium eum mollitia. Voluptatum, nobis, placeat.</p>
       </div>
     </section>
-    <section class="contact left">
-      <?= image_tag('contact.jpg', ['alt' => 'contact', 'class' => 'contentImg']) ?>
-    </section>
+    <section class="contact left"></section>
   </div>
 
   <!-- Right Side -->
@@ -40,9 +32,7 @@
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias quaerat itaque quo voluptatum fugit harum odit, dolorum nisi repellendus odio eveniet sapiente praesentium eius tempore sunt non rerum, delectus amet.</p>
       </div>
     </section>
-    <section class="info right">
-      <?= image_tag('about.jpg', ['alt' => 'portrait', 'class' => 'contentImg']) ?>
-    </section>
+    <section class="info right"></section>
     <section class="gigs right">
       <div class="content">
         <ul>
@@ -56,15 +46,11 @@
         </ul>
       </div>
     </section>
-    <section class="news right">
-      <?= image_tag('news.jpg', ['alt' => 'portrait', 'class' => 'contentImg']) ?>
-    </section>
+    <section class="news right"></section>
     <section class="listen right">
       <iframe width="100%" height="400" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/16702214&amp;color=bd3b20&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false"></iframe>
     </section>
-    <section class="welcome right">
-      <?= image_tag('home2.jpg', ['alt' => 'portrait', 'class' => 'contentImg']) ?>
-    </section>
+    <section class="welcome right"></section>
   </div><!-- EO#right -->
 </div>
 @stop
