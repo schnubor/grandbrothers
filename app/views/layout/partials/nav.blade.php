cec411982b0d8410851ee2fa32236696420dda66
schnubor <info@chko.org>
2015-08-14 00:47:02 +0200

    added mobile nav

--- app/views/layout/partials/nav.blade.php
+++ app/views/layout/partials/nav.blade.php
@@ -1,9 +1,19 @@
 <nav>
   <ul>
-    <li class="nav-news"><a href="#News" title="News about the Grandbrothers" class="page-scroll"><img src="/assets/nav/news.png" alt="News"></a></li>
-    <li class="nav-info"><a href="#Info" title="Read more about the Grandbrothers" class="page-scroll"><img src="/assets/nav/info.png" alt="Info"></a></li>
-    <li class="nav-music"><a href="#Music" title="Listen to samples" class="page-scroll"><img src="/assets/nav/music.png" alt="Music"></a></li>
-    <li class="nav-live"><a href="#Live" title="Upcoming gigs and concerts" class="page-scroll"><img src="/assets/nav/live.png" alt="Live"></a></li>
-    <li class="nav-contact"><a href="#Contact" title="Get in touch" class="page-scroll"><img src="/assets/nav/contact.png" alt="Contact"></a></li>
+        <li class="nav-news"><a href="#News" title="News about the Grandbrothers" class="page-scroll"><img src="/assets/nav/news.png" alt="News"></a></li>
+        <li class="nav-info"><a href="#Info" title="Read more about the Grandbrothers" class="page-scroll"><img src="/assets/nav/info.png" alt="Info"></a></li>
+        <li class="nav-music"><a href="#Music" title="Listen to samples" class="page-scroll"><img src="/assets/nav/music.png" alt="Music"></a></li>
+        <li class="nav-live"><a href="#Live" title="Upcoming gigs and concerts" class="page-scroll"><img src="/assets/nav/live.png" alt="Live"></a></li>
+        <li class="nav-contact"><a href="#Contact" title="Get in touch" class="page-scroll"><img src="/assets/nav/contact.png" alt="Contact"></a></li>
   </ul>
 </nav>
+
+<div class="mobile-nav">
+    <ul>
+        <li class="nav-news"><a href="#News" title="News about the Grandbrothers" class="page-scroll"><img src="/assets/nav/news.png" alt="News"></a></li>
+        <li class="nav-info"><a href="#Info" title="Read more about the Grandbrothers" class="page-scroll"><img src="/assets/nav/info.png" alt="Info"></a></li>
+        <li class="nav-live"><a href="#Music" title="Listen to samples" class="page-scroll"><img src="/assets/nav/music.png" alt="Music"></a></li>
+        <li class="nav-music"><a href="#Live" title="Upcoming gigs and concerts" class="page-scroll"><img src="/assets/nav/live.png" alt="Live"></a></li>
+        <li class="nav-contact"><a href="#Contact" title="Get in touch" class="page-scroll"><img src="/assets/nav/contact.png" alt="Contact"></a></li>
+    </ul>
+</div>
